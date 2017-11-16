071e0a01f59a41efd53966e7dcc0df94f49a92bf
Asad Tungeker <Asad.Tungeker@careerbuilder.com>
2017-11-16 20:20:43 +0500

    Initial Commit

---
+++ YourResume/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using YourResume.Models;
+
+namespace YourResume.Controllers
+{
+    public class HomeController : Controller
+    {
+        YourResumeEntities db = new YourResumeEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetDocumentFileName()
+        {
+            return Json(new
+            {
+                document = db.tblDocuments.FirstOrDefault().Name
+            }, 
+            JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ YourResume/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YourResume.MvcApplication" Language="C#" %>
